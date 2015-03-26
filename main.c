dc6c5333ddb9a7b5a61b430b6df9ca8edd423c14
rajnesh <rajneshraturi25@gmail.com>
2015-03-26 23:57:58 +0530

    base to read elf file format

---
+++ main.c
@@ -0,0 +1,59 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <malloc.h>
+#include <elf.h>
+
+int main()
+{
+  FILE* ElfFile = NULL;
+  char* SectNames = NULL;
+  int idx;
+  Elf64_Ehdr elf;
+   ElfFile = fopen("core", "r");
+   if(ElfFile == NULL) {
+      printf("Failed to open file\n");
+      return 0;
+   }
+   printf("Reading core file\n");
+   fread(&elf, 1, sizeof elf, ElfFile);
+   printf("%x", elf.e_ident[0]);
+
+   if((elf.e_ident[EI_MAG0] != ELFMAG0) 
+      && (elf.e_ident[EI_MAG1] != ELFMAG1)
+      && (elf.e_ident[EI_MAG2] != ELFMAG2)
+      && (elf.e_ident[EI_MAG3] != ELFMAG3)) {
+      printf("not an elf file format. giving up\n");
+      return 0;
+   }
+
+
+   if(elf.e_ident[EI_CLASS] == ELFCLASS64) {
+      printf("obj file is 64 bit\n");
+   }
+
+   if(elf.e_ident[EI_VERSION] != EV_CURRENT) {
+      printf("obj version is not current. giving up\n");
+      return 0;
+   }
+   switch(elf.e_type) {
+   case ET_NONE :
+      printf("not a supported file\n");
+      return 0;
+   case ET_EXEC:
+      printf("executable file\n");
+      break;
+   case ET_CORE:
+      printf("core dump\n");
+      break;
+   }
+
+   if(EM_X86_64 != elf.e_machine) {
+      printf("not a 64 bit obj file. giving up\n");
+      return 0;
+   }
+
+   if(elf.e_version != EV_CURRENT) {
+      printf("The obj version is not current. Giving up\n");
+   }
+   return 0; 
+}
